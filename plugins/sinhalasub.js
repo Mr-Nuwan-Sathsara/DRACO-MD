5f1c595b71ec8c5999698fac2942f1b07f0338d0
Mr-Nuwan-Sathsara <contact.nuwansathsara@gmail.com>
2024-10-16 23:40:35 +0530

    Update sinhalasub.js

--- plugins/sinhalasub.js
+++ plugins/sinhalasub.js
@@ -16,27 +16,23 @@ const url = `https://pirate.lk/?s=${q}`
 const response = await axios.get(url)
 const $ = cheerio.load(response.data)
 const noresult = $("#contenedor > div.module > div.content.rigth.csearch > div > div.no-result.animation-2 > h2").text()
-console.log(noresult)
 
    if(!q){
         await m.react("❌")
         return reply("*❗️ Give movie name to download subtitle. ❗️*")
-    }else if(noresult == `No results to show with `) {
+    }else if(noresult == `No results to show with ${q}`) {
         await m.react("❌")
         return reply("*❗️ Result not found!. ❗️*")
     }
 
     const url2 = $("#contenedor > div.module > div.content.rigth.csearch > div.search-page > div:nth-child(2) > article > div.details > div.title > a").attr('href')
-    console.log(url2)
     const response2 = await axios.get(url2)
     const $$ = cheerio.load(response2.data)
     const subpage = $$(`#link-5095 > td:nth-child(1) > a`).attr('href')
-    console.log(subpage)
 
     const response3 = await axios.get(subpage)
     const $$$ = cheerio.load(response3.data)
     const sublink = $$$("#link").attr('href')
-	console.log(sublink)
     
 }catch(e){
 console.log(e)
