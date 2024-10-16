422d28bd87f8e2787785bb6a4f6a029fafa5d605
Mr-Nuwan-Sathsara <contact.nuwansathsara@gmail.com>
2024-10-17 00:26:56 +0530

    Update sinhalasub.js

--- plugins/sinhalasub.js
+++ plugins/sinhalasub.js
@@ -65,12 +65,19 @@ console.log(noresult)
         return reply("*❗️ Subtitle not found!. ❗️*")
     }
     await m.react("🎬")
-    const url2 = $("body > div.elementor.elementor-234293.elementor-location-archive > section.elementor-section.elementor-top-section.elementor-element.elementor-element-70cfc451.elementor-section-full_width.elementor-section-stretched.elementor-section-height-default.elementor-section-height-default > div > div.elementor-column.elementor-col-66.elementor-top-column.elementor-element.elementor-element-1c0cc75 > div > div > div > div.elementor-posts-container.elementor-posts.elementor-posts--skin-cards.elementor-grid.elementor-has-item-ratio > article.elementor-post.elementor-grid-item.post-231332.post.type-post.status-publish.format-standard.has-post-thumbnail.hentry.category-18-plus.category-all.category-box-office-hits.category-coming-of-age.category-featured-articles.category-maxxa-1.category-sinhala-subtitle.category-crime.category-mystery.category-strange.category-ela.category-action.category-movies.category-thriller.category-drama.category-must-watch.category-war.category-adventure.category-shan-vimukthi.category-sinhala-subtitles.tag-cailee-spaeny.tag-kirsten-dunst.tag-sinhala-sub.tag-sinhala-subs.tag-sinhala-subtitle.tag-sinhala-subtitles.tag-sonoya-mizuno.tag-stephen-mckinley-henderson.tag-subs.tag-subtitle-2.tag-subtitles.tag-wagner-moura.tag-2160.tag-10630.tag-10695.tag-2135.tag-7575.tag-2134.tag-2196.tag-2133.tag-2159 > div > a").attr('href')
-    const response2 = await axios.get(url2)
+    const alllinks = []
+    const allmovies = $("body > div.elementor.elementor-234293.elementor-location-archive > section.elementor-section.elementor-top-section.elementor-element.elementor-element-70cfc451.elementor-section-full_width.elementor-section-stretched.elementor-section-height-default.elementor-section-height-default > div > div.elementor-column.elementor-col-66.elementor-top-column.elementor-element.elementor-element-1c0cc75 > div > div > div > div.elementor-posts-container.elementor-posts.elementor-posts--skin-cards.elementor-grid.elementor-has-item-ratio")
+    allmovies.each(function(){
+        url2 = $(this).find("article .elementor-post__card .elementor-post__text .elementor-post__title a").attr('href')
+
+        alllinks.push({url2})
+    })
+    console.log(alllinks)
+    /*const response2 = await axios.get(url2)
     const $$ = cheerio.load(response2.data)
     const subpageid = $$("#cm-primary > div.cm-posts.clearfix > article").attr('id')
     console.log(subpageid)
-    /*const subpage = $$(`#${subpageid} > td:nth-child(1) > a`).attr('href')
+    const subpage = $$(`#${subpageid} > td:nth-child(1) > a`).attr('href')
     const subname = $$$("body > div > div > div > div.inside > small:nth-child(4) > a").text().trim()
     const sublink = $$$("#link").attr('href')
 
