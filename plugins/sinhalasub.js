abb4831852cd02b85615f75e5123043a243448f3
Mr-Nuwan-Sathsara <contact.nuwansathsara@gmail.com>
2024-10-16 23:14:02 +0530

    Update sinhalasub.js

--- plugins/sinhalasub.js
+++ plugins/sinhalasub.js
@@ -12,24 +12,26 @@ cmd({
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
 
-const url = `https://cineru.lk/?s=${q}`
+const url = `https://www.baiscope.lk/?s=${q}`
 const response = await axios.get(url)
 const $ = cheerio.load(response.data)
-const noresult = $("#main-content > div.content > div.page-head > h2").text().trim()
+const noresult = $("body > div.elementor.elementor-234293.elementor-location-archive > section.elementor-section.elementor-top-section.elementor-element.elementor-element-70cfc451.elementor-section-full_width.elementor-section-stretched.elementor-section-height-default.elementor-section-height-default > div > div.elementor-column.elementor-col-66.elementor-top-column.elementor-element.elementor-element-1c0cc75 > div").text()
 
    if(!q){
         await m.react("❌")
         return reply("*❗️ Give movie name to download subtitle. ❗️*")
-    }else if(noresult == `Nothing Found`) {
+    }else if(noresult == `
+							`) {
         await m.react("❌")
         return reply("*❗️ Result not found!. ❗️*")
     }
 
-    const movie = $("#main-content > div.content > div.post-listing.archive-box > article:nth-child(1) > h2 > a").attr('href')
-    const response2 = await axios.get(movie)
+    const url2 = $("body > div.elementor.elementor-234293.elementor-location-archive > section.elementor-section.elementor-top-section.elementor-element.elementor-element-70cfc451.elementor-section-full_width.elementor-section-stretched.elementor-section-height-default.elementor-section-height-default > div > div.elementor-column.elementor-col-66.elementor-top-column.elementor-element.elementor-element-1c0cc75 > div > div > div > div > article.elementor-post.elementor-grid-item.post-115997.post.type-post.status-publish.format-standard.has-post-thumbnail.hentry.category-all.category-box-office-hits.category-featured-articles.category-sinhala-subtitle.category-crime.category-english.category-action.category-chamara-sampath.category-movies.category-thriller.category-you-decide.category-must-watch.category-sinhala-subtitles.tag-12659.tag-danny-huston.tag-frederick-schmidt.tag-gerard-butler.tag-morgan-freeman.tag-piper-perabo.tag-rocci-williams.tag-sinhala.tag-sinhala-subtitle.tag-10630.tag-2135.tag-2134.tag-2196 > div > div > h5 > a").attr('href')
+    console.log(url2)
+    const response2 = await axios.get(url2)
     const $$ = cheerio.load(response.data)
 
-    const sublink = $$("#btn-download").attr('data-link')
+    const sublink = $$("#post-115997 > div > div.cm-entry-summary > a").attr('href')
     console.log(sublink)
     
 }catch(e){
