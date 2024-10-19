c99af06b7a916308d9ad6cbb044d33778cb3f091
Mr-Nuwan-Sathsara <contact.nuwansathsara@gmail.com>
2024-10-19 20:26:02 +0530

    Update s.js

--- plugins/s.js
+++ plugins/s.js
@@ -37,22 +37,22 @@ movies.each(function(){
     mvdata.push({title,rating,year,link})
 })
 
+const movielist = `${mvdata[0].title} | ${mvdata[0].year}`
+if(mvdata[1].title) movielist += `\n\n${mvdata[1].title} | ${mvdata[1].year}`
+if(mvdata[2].title) movielist += `\n\n${mvdata[2].title} | ${mvdata[2].year}`
+if(mvdata[3].title) movielist += `\n\n${mvdata[3].title} | ${mvdata[3].year}`
+if(mvdata[4].title) movielist += `\n\n${mvdata[4].title} | ${mvdata[4].year}`
+if(mvdata[5].title) movielist += `\n\n${mvdata[5].title} | ${mvdata[5].year}`
+if(mvdata[6].title) movielist += `\n\n${mvdata[6].title} | ${mvdata[6].year}`
+if(mvdata[7].title) movielist += `\n\n${mvdata[7].title} | ${mvdata[7].year}`
+if(mvdata[8].title) movielist += `\n\n${mvdata[8].title} | ${mvdata[8].year}`
+    
 let desc = `
 
 surl :
 ${url}
-https://github.com/Mr-Nuwan-Sathsara/DRACO-MD
-https://github.com/Mr-Nuwan/DRACO-MD/edit/master
 
-mv1 = ${mvdata[0].title}
-
-mv2 = ${mvdata[1].title}
-
-mv3 = ${mvdata[2].title}
-
-mv4 = ${mvdata[3].title}
-
-mv5 = ${mvdata[4].title}
+${movielist}
 
 \n\n\n${botwatermark}`
 
@@ -73,8 +73,6 @@ async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender
 try{
 
 const url = urls[0]
-const urll = urls[1]
-const urlll = urls[2]
 const response = await axios.get(url)
 const $ = cheerio.load(response.data)
 
@@ -136,10 +134,6 @@ ${p480}
 ${sub}\n\n\n${botwatermark}`
 
 await conn.sendMessage(from,{image: {url: img},caption: desc},{quoted: mek})
-
-console.log(url)
-console.log(urll)
-console.log(urlll)
 
 }catch(e){
 console.log(e)
@@ -203,6 +197,552 @@ let desc = `
 *⭐️ Rating :* ${mvdata[1].rating}
 *📆 Year :* ${mvdata[1].year}
 *🔗 Url :* ${mvdata[1].link}
+
+*📥 Download Links 📥*
+*1️⃣ 1080P(${ms21080}${gbormb1080}) :*
+${p1080}
+
+*2️⃣ 720P(${ms2720}${gbormb720}) :*
+${p720}
+
+*3️⃣ 480P(${ms2480}${gbormb480}) :*
+${p480}
+
+*4️⃣ Sinhala Subtitle :*
+${sub}\n\n\n${botwatermark}`
+
+await conn.sendMessage(from,{image: {url: img},caption: desc},{quoted: mek})
+
+}catch(e){
+console.log(e)
+reply(`${e}`)
+}
+})
+
+cmd({
+    reply_pattern: "3",
+    quoted_includes: "surl :",
+    filename: __filename
+},
+async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply, urls}) => {
+try{
+
+const url = urls[0]
+const response = await axios.get(url)
+const $ = cheerio.load(response.data)
+const resulterror = $("#contenedor > div.module > div.content.rigth.csearch > div > div.no-result.animation-2 > h2").text()
+
+await m.react("🎬")
+const mvdata = []
+const movies = $("article")
+movies.each(function(){
+    const title = $(this).find(".title a").text()
+    const rating = $(this).find(".rating").text()
+    const year = $(this).find(".year").text()
+    const link = $(this).find(".title a").attr('href')
+
+    mvdata.push({title,rating,year,link})
+})
+
+const url2 = mvdata[2].link
+const response2 = await axios.get(url2)
+const $$ = cheerio.load(response2.data)
+const p1080 = $$("tr:nth-child(1) > td:nth-child(1) > a").attr('href')
+const ms1080 = $$("tr:nth-child(1) > td:nth-child(3)").text()
+const ms21080 = ms1080.split(" ")[0]
+const ms31080 = ms1080.search("MB")
+const p720 = $$("tr:nth-child(2) > td:nth-child(1) > a").attr('href')
+const ms720 = $$("tr:nth-child(2) > td:nth-child(3)").text()
+const ms2720 = ms720.split(" ")[0]
+const ms3720 = ms720.search("MB")
+const p480 = $$("tr:nth-child(3) > td:nth-child(1) > a").attr('href')
+const ms480 = $$("tr:nth-child(3) > td:nth-child(3)").text()
+const ms2480 = ms480.split(" ")[0]
+const ms3480 = ms480.search("MB")
+const sub = $$("tbody:nth-child(2) > tr:nth-child(1) > td:nth-child(1) > a").attr('href')
+const img = $$("#single > div.content.right > div.sheader > div.poster > img").attr('src')
+
+let gbormb1080 = `MB`
+let gbormb720 = `MB`
+let gbormb480 = `MB`
+
+if(ms31080 == -1) gbormb1080 = `GB`
+if(ms3720 == -1) gbormb720 = `GB`
+if(ms3480 == -1) gbormb480 = `GB`
+
+let desc = `
+*📝 Title :* ${mvdata[2].title}
+*⭐️ Rating :* ${mvdata[2].rating}
+*📆 Year :* ${mvdata[2].year}
+*🔗 Url :* ${mvdata[2].link}
+
+*📥 Download Links 📥*
+*1️⃣ 1080P(${ms21080}${gbormb1080}) :*
+${p1080}
+
+*2️⃣ 720P(${ms2720}${gbormb720}) :*
+${p720}
+
+*3️⃣ 480P(${ms2480}${gbormb480}) :*
+${p480}
+
+*4️⃣ Sinhala Subtitle :*
+${sub}\n\n\n${botwatermark}`
+
+await conn.sendMessage(from,{image: {url: img},caption: desc},{quoted: mek})
+
+}catch(e){
+console.log(e)
+reply(`${e}`)
+}
+})
+
+cmd({
+    reply_pattern: "4",
+    quoted_includes: "surl :",
+    filename: __filename
+},
+async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply, urls}) => {
+try{
+
+const url = urls[0]
+const response = await axios.get(url)
+const $ = cheerio.load(response.data)
+const resulterror = $("#contenedor > div.module > div.content.rigth.csearch > div > div.no-result.animation-2 > h2").text()
+
+await m.react("🎬")
+const mvdata = []
+const movies = $("article")
+movies.each(function(){
+    const title = $(this).find(".title a").text()
+    const rating = $(this).find(".rating").text()
+    const year = $(this).find(".year").text()
+    const link = $(this).find(".title a").attr('href')
+
+    mvdata.push({title,rating,year,link})
+})
+
+const url2 = mvdata[3].link
+const response2 = await axios.get(url2)
+const $$ = cheerio.load(response2.data)
+const p1080 = $$("tr:nth-child(1) > td:nth-child(1) > a").attr('href')
+const ms1080 = $$("tr:nth-child(1) > td:nth-child(3)").text()
+const ms21080 = ms1080.split(" ")[0]
+const ms31080 = ms1080.search("MB")
+const p720 = $$("tr:nth-child(2) > td:nth-child(1) > a").attr('href')
+const ms720 = $$("tr:nth-child(2) > td:nth-child(3)").text()
+const ms2720 = ms720.split(" ")[0]
+const ms3720 = ms720.search("MB")
+const p480 = $$("tr:nth-child(3) > td:nth-child(1) > a").attr('href')
+const ms480 = $$("tr:nth-child(3) > td:nth-child(3)").text()
+const ms2480 = ms480.split(" ")[0]
+const ms3480 = ms480.search("MB")
+const sub = $$("tbody:nth-child(2) > tr:nth-child(1) > td:nth-child(1) > a").attr('href')
+const img = $$("#single > div.content.right > div.sheader > div.poster > img").attr('src')
+
+let gbormb1080 = `MB`
+let gbormb720 = `MB`
+let gbormb480 = `MB`
+
+if(ms31080 == -1) gbormb1080 = `GB`
+if(ms3720 == -1) gbormb720 = `GB`
+if(ms3480 == -1) gbormb480 = `GB`
+
+let desc = `
+*📝 Title :* ${mvdata[3].title}
+*⭐️ Rating :* ${mvdata[3].rating}
+*📆 Year :* ${mvdata[3].year}
+*🔗 Url :* ${mvdata[3].link}
+
+*📥 Download Links 📥*
+*1️⃣ 1080P(${ms21080}${gbormb1080}) :*
+${p1080}
+
+*2️⃣ 720P(${ms2720}${gbormb720}) :*
+${p720}
+
+*3️⃣ 480P(${ms2480}${gbormb480}) :*
+${p480}
+
+*4️⃣ Sinhala Subtitle :*
+${sub}\n\n\n${botwatermark}`
+
+await conn.sendMessage(from,{image: {url: img},caption: desc},{quoted: mek})
+
+}catch(e){
+console.log(e)
+reply(`${e}`)
+}
+})
+
+cmd({
+    reply_pattern: "5",
+    quoted_includes: "surl :",
+    filename: __filename
+},
+async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply, urls}) => {
+try{
+
+const url = urls[0]
+const response = await axios.get(url)
+const $ = cheerio.load(response.data)
+const resulterror = $("#contenedor > div.module > div.content.rigth.csearch > div > div.no-result.animation-2 > h2").text()
+
+await m.react("🎬")
+const mvdata = []
+const movies = $("article")
+movies.each(function(){
+    const title = $(this).find(".title a").text()
+    const rating = $(this).find(".rating").text()
+    const year = $(this).find(".year").text()
+    const link = $(this).find(".title a").attr('href')
+
+    mvdata.push({title,rating,year,link})
+})
+
+const url2 = mvdata[4].link
+const response2 = await axios.get(url2)
+const $$ = cheerio.load(response2.data)
+const p1080 = $$("tr:nth-child(1) > td:nth-child(1) > a").attr('href')
+const ms1080 = $$("tr:nth-child(1) > td:nth-child(3)").text()
+const ms21080 = ms1080.split(" ")[0]
+const ms31080 = ms1080.search("MB")
+const p720 = $$("tr:nth-child(2) > td:nth-child(1) > a").attr('href')
+const ms720 = $$("tr:nth-child(2) > td:nth-child(3)").text()
+const ms2720 = ms720.split(" ")[0]
+const ms3720 = ms720.search("MB")
+const p480 = $$("tr:nth-child(3) > td:nth-child(1) > a").attr('href')
+const ms480 = $$("tr:nth-child(3) > td:nth-child(3)").text()
+const ms2480 = ms480.split(" ")[0]
+const ms3480 = ms480.search("MB")
+const sub = $$("tbody:nth-child(2) > tr:nth-child(1) > td:nth-child(1) > a").attr('href')
+const img = $$("#single > div.content.right > div.sheader > div.poster > img").attr('src')
+
+let gbormb1080 = `MB`
+let gbormb720 = `MB`
+let gbormb480 = `MB`
+
+if(ms31080 == -1) gbormb1080 = `GB`
+if(ms3720 == -1) gbormb720 = `GB`
+if(ms3480 == -1) gbormb480 = `GB`
+
+let desc = `
+*📝 Title :* ${mvdata[4].title}
+*⭐️ Rating :* ${mvdata[4].rating}
+*📆 Year :* ${mvdata[4].year}
+*🔗 Url :* ${mvdata[4].link}
+
+*📥 Download Links 📥*
+*1️⃣ 1080P(${ms21080}${gbormb1080}) :*
+${p1080}
+
+*2️⃣ 720P(${ms2720}${gbormb720}) :*
+${p720}
+
+*3️⃣ 480P(${ms2480}${gbormb480}) :*
+${p480}
+
+*4️⃣ Sinhala Subtitle :*
+${sub}\n\n\n${botwatermark}`
+
+await conn.sendMessage(from,{image: {url: img},caption: desc},{quoted: mek})
+
+}catch(e){
+console.log(e)
+reply(`${e}`)
+}
+})
+
+cmd({
+    reply_pattern: "6",
+    quoted_includes: "surl :",
+    filename: __filename
+},
+async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply, urls}) => {
+try{
+
+const url = urls[0]
+const response = await axios.get(url)
+const $ = cheerio.load(response.data)
+const resulterror = $("#contenedor > div.module > div.content.rigth.csearch > div > div.no-result.animation-2 > h2").text()
+
+await m.react("🎬")
+const mvdata = []
+const movies = $("article")
+movies.each(function(){
+    const title = $(this).find(".title a").text()
+    const rating = $(this).find(".rating").text()
+    const year = $(this).find(".year").text()
+    const link = $(this).find(".title a").attr('href')
+
+    mvdata.push({title,rating,year,link})
+})
+
+const url2 = mvdata[5].link
+const response2 = await axios.get(url2)
+const $$ = cheerio.load(response2.data)
+const p1080 = $$("tr:nth-child(1) > td:nth-child(1) > a").attr('href')
+const ms1080 = $$("tr:nth-child(1) > td:nth-child(3)").text()
+const ms21080 = ms1080.split(" ")[0]
+const ms31080 = ms1080.search("MB")
+const p720 = $$("tr:nth-child(2) > td:nth-child(1) > a").attr('href')
+const ms720 = $$("tr:nth-child(2) > td:nth-child(3)").text()
+const ms2720 = ms720.split(" ")[0]
+const ms3720 = ms720.search("MB")
+const p480 = $$("tr:nth-child(3) > td:nth-child(1) > a").attr('href')
+const ms480 = $$("tr:nth-child(3) > td:nth-child(3)").text()
+const ms2480 = ms480.split(" ")[0]
+const ms3480 = ms480.search("MB")
+const sub = $$("tbody:nth-child(2) > tr:nth-child(1) > td:nth-child(1) > a").attr('href')
+const img = $$("#single > div.content.right > div.sheader > div.poster > img").attr('src')
+
+let gbormb1080 = `MB`
+let gbormb720 = `MB`
+let gbormb480 = `MB`
+
+if(ms31080 == -1) gbormb1080 = `GB`
+if(ms3720 == -1) gbormb720 = `GB`
+if(ms3480 == -1) gbormb480 = `GB`
+
+let desc = `
+*📝 Title :* ${mvdata[5].title}
+*⭐️ Rating :* ${mvdata[5].rating}
+*📆 Year :* ${mvdata[5].year}
+*🔗 Url :* ${mvdata[5].link}
+
+*📥 Download Links 📥*
+*1️⃣ 1080P(${ms21080}${gbormb1080}) :*
+${p1080}
+
+*2️⃣ 720P(${ms2720}${gbormb720}) :*
+${p720}
+
+*3️⃣ 480P(${ms2480}${gbormb480}) :*
+${p480}
+
+*4️⃣ Sinhala Subtitle :*
+${sub}\n\n\n${botwatermark}`
+
+await conn.sendMessage(from,{image: {url: img},caption: desc},{quoted: mek})
+
+}catch(e){
+console.log(e)
+reply(`${e}`)
+}
+})
+
+cmd({
+    reply_pattern: "7",
+    quoted_includes: "surl :",
+    filename: __filename
+},
+async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply, urls}) => {
+try{
+
+const url = urls[0]
+const response = await axios.get(url)
+const $ = cheerio.load(response.data)
+const resulterror = $("#contenedor > div.module > div.content.rigth.csearch > div > div.no-result.animation-2 > h2").text()
+
+await m.react("🎬")
+const mvdata = []
+const movies = $("article")
+movies.each(function(){
+    const title = $(this).find(".title a").text()
+    const rating = $(this).find(".rating").text()
+    const year = $(this).find(".year").text()
+    const link = $(this).find(".title a").attr('href')
+
+    mvdata.push({title,rating,year,link})
+})
+
+const url2 = mvdata[6].link
+const response2 = await axios.get(url2)
+const $$ = cheerio.load(response2.data)
+const p1080 = $$("tr:nth-child(1) > td:nth-child(1) > a").attr('href')
+const ms1080 = $$("tr:nth-child(1) > td:nth-child(3)").text()
+const ms21080 = ms1080.split(" ")[0]
+const ms31080 = ms1080.search("MB")
+const p720 = $$("tr:nth-child(2) > td:nth-child(1) > a").attr('href')
+const ms720 = $$("tr:nth-child(2) > td:nth-child(3)").text()
+const ms2720 = ms720.split(" ")[0]
+const ms3720 = ms720.search("MB")
+const p480 = $$("tr:nth-child(3) > td:nth-child(1) > a").attr('href')
+const ms480 = $$("tr:nth-child(3) > td:nth-child(3)").text()
+const ms2480 = ms480.split(" ")[0]
+const ms3480 = ms480.search("MB")
+const sub = $$("tbody:nth-child(2) > tr:nth-child(1) > td:nth-child(1) > a").attr('href')
+const img = $$("#single > div.content.right > div.sheader > div.poster > img").attr('src')
+
+let gbormb1080 = `MB`
+let gbormb720 = `MB`
+let gbormb480 = `MB`
+
+if(ms31080 == -1) gbormb1080 = `GB`
+if(ms3720 == -1) gbormb720 = `GB`
+if(ms3480 == -1) gbormb480 = `GB`
+
+let desc = `
+*📝 Title :* ${mvdata[6].title}
+*⭐️ Rating :* ${mvdata[6].rating}
+*📆 Year :* ${mvdata[6].year}
+*🔗 Url :* ${mvdata[6].link}
+
+*📥 Download Links 📥*
+*1️⃣ 1080P(${ms21080}${gbormb1080}) :*
+${p1080}
+
+*2️⃣ 720P(${ms2720}${gbormb720}) :*
+${p720}
+
+*3️⃣ 480P(${ms2480}${gbormb480}) :*
+${p480}
+
+*4️⃣ Sinhala Subtitle :*
+${sub}\n\n\n${botwatermark}`
+
+await conn.sendMessage(from,{image: {url: img},caption: desc},{quoted: mek})
+
+}catch(e){
+console.log(e)
+reply(`${e}`)
+}
+})
+
+cmd({
+    reply_pattern: "8",
+    quoted_includes: "surl :",
+    filename: __filename
+},
+async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply, urls}) => {
+try{
+
+const url = urls[0]
+const response = await axios.get(url)
+const $ = cheerio.load(response.data)
+const resulterror = $("#contenedor > div.module > div.content.rigth.csearch > div > div.no-result.animation-2 > h2").text()
+
+await m.react("🎬")
+const mvdata = []
+const movies = $("article")
+movies.each(function(){
+    const title = $(this).find(".title a").text()
+    const rating = $(this).find(".rating").text()
+    const year = $(this).find(".year").text()
+    const link = $(this).find(".title a").attr('href')
+
+    mvdata.push({title,rating,year,link})
+})
+
+const url2 = mvdata[7].link
+const response2 = await axios.get(url2)
+const $$ = cheerio.load(response2.data)
+const p1080 = $$("tr:nth-child(1) > td:nth-child(1) > a").attr('href')
+const ms1080 = $$("tr:nth-child(1) > td:nth-child(3)").text()
+const ms21080 = ms1080.split(" ")[0]
+const ms31080 = ms1080.search("MB")
+const p720 = $$("tr:nth-child(2) > td:nth-child(1) > a").attr('href')
+const ms720 = $$("tr:nth-child(2) > td:nth-child(3)").text()
+const ms2720 = ms720.split(" ")[0]
+const ms3720 = ms720.search("MB")
+const p480 = $$("tr:nth-child(3) > td:nth-child(1) > a").attr('href')
+const ms480 = $$("tr:nth-child(3) > td:nth-child(3)").text()
+const ms2480 = ms480.split(" ")[0]
+const ms3480 = ms480.search("MB")
+const sub = $$("tbody:nth-child(2) > tr:nth-child(1) > td:nth-child(1) > a").attr('href')
+const img = $$("#single > div.content.right > div.sheader > div.poster > img").attr('src')
+
+let gbormb1080 = `MB`
+let gbormb720 = `MB`
+let gbormb480 = `MB`
+
+if(ms31080 == -1) gbormb1080 = `GB`
+if(ms3720 == -1) gbormb720 = `GB`
+if(ms3480 == -1) gbormb480 = `GB`
+
+let desc = `
+*📝 Title :* ${mvdata[7].title}
+*⭐️ Rating :* ${mvdata[7].rating}
+*📆 Year :* ${mvdata[7].year}
+*🔗 Url :* ${mvdata[7].link}
+
+*📥 Download Links 📥*
+*1️⃣ 1080P(${ms21080}${gbormb1080}) :*
+${p1080}
+
+*2️⃣ 720P(${ms2720}${gbormb720}) :*
+${p720}
+
+*3️⃣ 480P(${ms2480}${gbormb480}) :*
+${p480}
+
+*4️⃣ Sinhala Subtitle :*
+${sub}\n\n\n${botwatermark}`
+
+await conn.sendMessage(from,{image: {url: img},caption: desc},{quoted: mek})
+
+}catch(e){
+console.log(e)
+reply(`${e}`)
+}
+})
+
+cmd({
+    reply_pattern: "9",
+    quoted_includes: "surl :",
+    filename: __filename
+},
+async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply, urls}) => {
+try{
+
+const url = urls[0]
+const response = await axios.get(url)
+const $ = cheerio.load(response.data)
+const resulterror = $("#contenedor > div.module > div.content.rigth.csearch > div > div.no-result.animation-2 > h2").text()
+
+await m.react("🎬")
+const mvdata = []
+const movies = $("article")
+movies.each(function(){
+    const title = $(this).find(".title a").text()
+    const rating = $(this).find(".rating").text()
+    const year = $(this).find(".year").text()
+    const link = $(this).find(".title a").attr('href')
+
+    mvdata.push({title,rating,year,link})
+})
+
+const url2 = mvdata[8].link
+const response2 = await axios.get(url2)
+const $$ = cheerio.load(response2.data)
+const p1080 = $$("tr:nth-child(1) > td:nth-child(1) > a").attr('href')
+const ms1080 = $$("tr:nth-child(1) > td:nth-child(3)").text()
+const ms21080 = ms1080.split(" ")[0]
+const ms31080 = ms1080.search("MB")
+const p720 = $$("tr:nth-child(2) > td:nth-child(1) > a").attr('href')
+const ms720 = $$("tr:nth-child(2) > td:nth-child(3)").text()
+const ms2720 = ms720.split(" ")[0]
+const ms3720 = ms720.search("MB")
+const p480 = $$("tr:nth-child(3) > td:nth-child(1) > a").attr('href')
+const ms480 = $$("tr:nth-child(3) > td:nth-child(3)").text()
+const ms2480 = ms480.split(" ")[0]
+const ms3480 = ms480.search("MB")
+const sub = $$("tbody:nth-child(2) > tr:nth-child(1) > td:nth-child(1) > a").attr('href')
+const img = $$("#single > div.content.right > div.sheader > div.poster > img").attr('src')
+
+let gbormb1080 = `MB`
+let gbormb720 = `MB`
+let gbormb480 = `MB`
+
+if(ms31080 == -1) gbormb1080 = `GB`
+if(ms3720 == -1) gbormb720 = `GB`
+if(ms3480 == -1) gbormb480 = `GB`
+
+let desc = `
+*📝 Title :* ${mvdata[8].title}
+*⭐️ Rating :* ${mvdata[8].rating}
+*📆 Year :* ${mvdata[8].year}
+*🔗 Url :* ${mvdata[8].link}
 
 *📥 Download Links 📥*
 *1️⃣ 1080P(${ms21080}${gbormb1080}) :*
