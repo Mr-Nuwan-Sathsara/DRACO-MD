1784dcde2aecb48604b9ee6941e68f570bbc09d6
Mr-Nuwan-Sathsara <contact.nuwansathsara@gmail.com>
2024-10-20 23:32:48 +0530

    Update imdb.js

--- plugins/imdb.js
+++ plugins/imdb.js
@@ -16,9 +16,9 @@ try{
   const response = await axios.get(url)
   const $ = cheerio.load(response.data)
   const movies = []
-  const allmovies = $("#__next > main > div.ipc-page-content-container.ipc-page-content-container--full.sc-253ad8fd-0.iFnBOf > div.ipc-page-content-container.ipc-page-content-container--center > section > div > div.ipc-page-grid__item.ipc-page-grid__item--span-2 > section:nth-child(4) > div.sc-e8e4ce7-2.gdpdyr > ul")
+  const allmovies = $("ul")
   allmovies.each(function(){
-    title = $(this).find(".ipc-metadata-list-summary-item__t").text()
+    title = $(this).find("li div.ipc-metadata-list-summary-item__c div a").text()
 
     movies.push({title})
   })
