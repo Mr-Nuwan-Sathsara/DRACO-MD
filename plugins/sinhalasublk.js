7ac3945ab1ae2ec0f578e4b5c6827e96d84a34be
Mr-Nuwan-Sathsara <contact.nuwansathsara@gmail.com>
2024-10-20 08:58:10 +0530

    Update sinhalasublk.js

--- plugins/sinhalasublk.js
+++ plugins/sinhalasublk.js
@@ -91,8 +91,8 @@ movies.each(function(){
 })
     
 await m.react("🎬")
-
 const url2 = mvdata[0].link
+if(!mvdata[1]) url2 = $("#contenedor > div.module > div.content.rigth.csearch > div > div.result-item > article > div.details > div.title > a").attr('href')
 const response2 = await axios.get(url2)
 const $$ = cheerio.load(response2.data)
 const ms1080 = $$("tr:nth-child(1) > td:nth-child(3)").text()
