f1be7d6b96039a6c6615b3e83a34f1144284b009
Mr-Nuwan-Sathsara <contact.nuwansathsara@gmail.com>
2024-10-23 21:11:42 +0530

    Update aaa.js

--- plugins/aaa.js
+++ plugins/aaa.js
@@ -13,8 +13,13 @@ cmd({
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
 
-await conn.sendMessage(from, {document: {url: `https://pixeldrain.com/api/file/dweL8D6g`},fileName: `Movie Ekah.mp4`,mimetype: "video/mp4",caption: botwatermark},{quoted: mek})
+    const url = `https://cinesubz.co/api-rwjdzuehbdrwjdzuehbdzjyvxo2bhh0azjyvxo2bhh0auehbdruehbdrwjdzuehbdzjyvxo2bhh0azjyvxo2bhh0auehbdrwjdzuehbwjdzuehbdzjyvxo2bhh0azjyvxo2bhh0a/hqmedxocei/`
+    const response = await axios.get(url)
+    const $ = cheerio.load(response.data)
 
+    const dlurl = $("#link").attr('href')
+    console.log(dlurl)
+    
 }catch(e){
 console.log(e)
 reply(`${e}`)
