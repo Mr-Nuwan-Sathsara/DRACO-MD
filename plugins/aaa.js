29e5518aa560edecc47663675ccc88fbd70a8f0f
Mr-Nuwan-Sathsara <contact.nuwansathsara@gmail.com>
2024-10-23 21:34:09 +0530

    Update aaa.js

--- plugins/aaa.js
+++ plugins/aaa.js
@@ -13,18 +13,18 @@ cmd({
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
 
-    const url = `https://cinesubz.co/api-rwjdzuehbdrwjdzuehbdzjyvxo2bhh0azjyvxo2bhh0auehbdruehbdrwjdzuehbdzjyvxo2bhh0azjyvxo2bhh0auehbdrwjdzuehbwjdzuehbdzjyvxo2bhh0azjyvxo2bhh0a/wzheltoln7/`
+    /*const url = `https://cinesubz.co/api-rwjdzuehbdrwjdzuehbdzjyvxo2bhh0azjyvxo2bhh0auehbdruehbdrwjdzuehbdzjyvxo2bhh0azjyvxo2bhh0auehbdrwjdzuehbwjdzuehbdzjyvxo2bhh0azjyvxo2bhh0a/wzheltoln7/`
     const response = await axios.get(url)
     const $ = cheerio.load(response.data)
-
-    const dlurl = $("#link").attr('href')
+*/
+    const dlurl = "https://google.com/server4/1:/tnganuwpxppzfymytsqr/202403/The.Tomorrow.War.2021.WEBRip-%5BCineSubz.co%5D-480p.mp4"
+    console.log(dlurl)
     const sl = dlurl.search("https://google.com/server4/1:/")
+    console.log(sl)
     if(!sl == -1) {
         const dlurl2 = dlurl.replace("https://google.com/server4/1:/","https://ima04.cskinglk.xyz/server4/")
         console.log(dlurl2)
     }
-    
-    console.log(dlurl)
     
 }catch(e){
 console.log(e)
