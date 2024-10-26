a49355b06e508205c1e3455f874d0d09339729a5
Mr-Nuwan-Sathsara <contact.nuwansathsara@gmail.com>
2024-10-26 09:57:59 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,5 +6,5 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "7dMjHCTR#6ioXgVo6e2gW4VQNnI3ligEIoh4UaRj5zGeEZziZz_o",
-MONGODB: process.env.MONGODB || "mongodb://mongo:CUdNvGBUzCFIfaXKanmzBPQkXuhmfBZA@junction.proxy.rlwy.net:10188",
+MONGODB: process.env.MONGODB || "mongodb+srv://tesil69892:geftVnd17Jt1ct7a@cluster0.lgcsm.mongodb.net/",
 };
